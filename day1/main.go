8beeaa574f763f6402ea38a95ae69ef5610c656f
Eric Trinque <solvdinc@outlook.com>
2024-12-11 18:15:53 -0500

    -broken

--- day1/main.go
+++ day1/main.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-var input, _ = os.ReadFile("./input")
+var input, _ = os.ReadFile("./input2")
 
 func main() {
 
@@ -16,46 +16,63 @@ func main() {
 	var right []int
 
 	input = bytes.ReplaceAll(input, []byte("\n"), []byte(" "))
+
+	fmt.Println("Input", input)
+
 	inputMatrix := bytes.Split(input, []byte(" "))
+
+	fmt.Println("InputMatrix", inputMatrix)
 
 	intList := convInt(inputMatrix)
 
+	fmt.Println("IntList", intList)
+
 	left, right = splitInput(intList)
+	//sortNums(left)
+	//sortNums(right)
+	fmt.Printf("Left:\n %d\n\n", left)
+	fmt.Printf("Right:\n %d\n\n", right)
 
-	sortNums(left)
-	sortNums(right)
+	//fmt.Printf("The distance between the two lists is: %d\n", solve(left, right))
+	//fmt.Printf("The similarity score is: %d", calcSimScore(left, right))
 
-	//fmt.Printf("Left:\n %d\n\n", left)
-	//fmt.Printf("Right:\n %d\n\n", right)
-	fmt.Printf("The distance between the two lists is: %d\n", solve(left, right))
 }
 
 func convInt(list [][]byte) []int {
 	var nums []int
+	//var numsList [][]int
+	fmt.Printf("List Length: %d\n", len(list))
 
-	for i := range list {
+	for _, i := range list {
 		var str string
 
-		for _, j := range list[i] {
-			if j == ' ' {
+		if i == nil {
+			continue
+		}
+
+		for _, j := range i {
+			if j == byte(' ') {
 				continue
 			}
 			str += string(j)
 
 		}
+
 		num, _ := strconv.Atoi(str)
 		if num == 0 {
 			continue
 		}
+
 		nums = append(nums, num)
 	}
 	return nums
 }
 
 func splitInput(nums []int) ([]int, []int) {
+
 	var nums1, nums2 []int
 
-	for i, j := 0, 1; j < len(nums); {
+	for i, j := 0, 1; j < len(nums)-1; {
 		nums1 = append(nums1, nums[i])
 		nums2 = append(nums2, nums[j])
 		i += 2
@@ -92,4 +109,35 @@ func diff(a, b int) int {
 		return a - b
 	}
 	return b - a
+}
+
+/*
+PART 2:
+
+This time, you'll need to figure out exactly how often each number from the left list appears in the right list.
+Calculate a total similarity score by adding up each number in the left list
+after multiplying it by the number of times that number appears in the right list.
+
+*/
+
+func calcSimScore(nums1, nums2 []int) int {
+	// naive solution. could be done with map
+	var accum int
+
+	for i := range nums1 {
+		var count int
+		for j := range nums2 {
+			if nums1[i] == nums2[j] {
+				count += 1
+			} else {
+				continue
+			}
+		}
+		if count > 0 {
+			accum += count * nums1[i]
+		} else {
+			accum += nums1[i]
+		}
+	}
+	return accum
 }
--- day2/main.go
+++ day2/main.go
@@ -4,26 +4,29 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 var input, _ = os.ReadFile("./input")
 
 func main() {
 
-	//mod to work with delimited ints, use \n as slice delimiter
+	//mod to work with delimited int, use \n as slice delimiter
 	var arr []int
+
+	//parse slice into string, if == 32 continue else conv to str and concat then append back to array
 
 	func() {
 		inputMatrix := bytes.Split(input, []byte("\n"))
 		for i := 0; i < len(inputMatrix); i++ {
-			for j := 0; j < len(inputMatrix[i]); j++ {
-				if inputMatrix[i][j] == ' ' {
-					continue
-				}
-				num, _ := strconv.Atoi(string(inputMatrix[i][j]))
-				arr = append(arr, num)
-			}
+			cut := bytes.TrimSpace(inputMatrix[i])
+			fmt.Println(cut)
+			//for j := 0; j < len(inputMatrix[i]); j++ {
+			//	if inputMatrix[i][j] == ' ' {
+			//		continue
+			//	}
+			//	num, _ := strconv.Atoi(string(inputMatrix[i][j]))
+			//	arr = append(arr, num)
+			//}
 		}
 		fmt.Println(arr)
 	}()
