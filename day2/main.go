ac584791b14d9fc5a6b95c228bf9da74f60fc296
Eric Trinque <solvdinc@outlook.com>
2024-12-12 18:10:49 -0500

    -

--- day2/main.go
+++ day2/main.go
@@ -11,15 +11,17 @@ var input, _ = os.ReadFile("./input")
 func main() {
 
 	//mod to work with delimited int, use \n as slice delimiter
-	var arr []int
+	//var arr []int
 
 	//parse slice into string, if == 32 continue else conv to str and concat then append back to array
 
 	func() {
 		inputMatrix := bytes.Split(input, []byte("\n"))
-		for i := 0; i < len(inputMatrix); i++ {
-			cut := bytes.TrimSpace(inputMatrix[i])
-			fmt.Println(cut)
+
+		for i := 0; i < 20; i++ {
+			fmt.Printf("String: %s\n", string(inputMatrix[i]))
+			fmt.Printf("Bytes: %d\n", inputMatrix[i])
+
 			//for j := 0; j < len(inputMatrix[i]); j++ {
 			//	if inputMatrix[i][j] == ' ' {
 			//		continue
@@ -28,6 +30,6 @@ func main() {
 			//	arr = append(arr, num)
 			//}
 		}
-		fmt.Println(arr)
+
 	}()
 }
